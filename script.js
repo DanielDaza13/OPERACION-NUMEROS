b181b2c5d33d658eb96889a231f637f9bdb34a0d
Carlos97135gr <56747317+Carlos97135gr@users.noreply.github.com>
2020-07-15 14:18:38 -0500

    version 1
    primera version del javascript en donde realiza la suma

---
+++ script.js
@@ -0,0 +1,9 @@
+function suma()
+{
+    var num1=Number(document.getElementById('text1').value);
+    var num2=Number(document.getElementById('text2').value);
+    var res= num1 + num2;
+    document.getElementById('res').value=res;
+
+
+}
